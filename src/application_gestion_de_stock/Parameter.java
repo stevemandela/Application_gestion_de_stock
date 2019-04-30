381bba2ab7a60bffedc4403220ea4769eda17a5c
stevemandela <stevemandelawandji93@gmail.com>
2019-05-01 01:11:31 +0200

    gestion de stock

---
+++ src/application_gestion_de_stock/Parameter.java
@@ -0,0 +1,14 @@
+
+package application_gestion_de_stock;
+
+public class Parameter {
+    
+       public static String IPHOST = "127.0.0.1";//adresse de bouclage
+       
+       public static String HOST_DB = "jdbc:mysql://" + IPHOST + ":3306/gestion_de_stock";
+       public static String USERNAME_DB = "root"; 
+       public static String PASSWORD_DB = "";//"root"
+       
+       public static int PORT = 11111;
+       public static String USER;
+}
